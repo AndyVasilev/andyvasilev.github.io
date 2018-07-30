36a4d29ede0c0d24cbd08ab2ecfd0fdff376377c
Andy Vasilev <Andy.vasilev@gmail.com>
2018-07-30 14:12:16 +0300

    b2b

--- js/index.js
+++ js/index.js
@@ -72,3 +72,67 @@ var swiper = new Swiper('.section-2 .swiper-container', {
         }
     }
 });
+
+
+//
+
+/*Калькулятор */
+/*Инициализация слайдеров*/
+var moneySlider = document.getElementById("slider-money");
+var dateSlider = document.getElementById("slider-date");
+
+noUiSlider.create(moneySlider, {
+	animate: true,
+	animationDuration: 300,
+	start: [0],
+	step: 300000,
+	range: {
+		min: [0],
+		max: [2000000]
+	}
+});
+noUiSlider.create(dateSlider, {
+	animate: true,
+	animationDuration: 300,
+	start: [0],
+	step: 1,
+	range: {
+		min: [0],
+		max: [10]
+	}
+});
+
+/*переменные для полей вывода чисел*/
+var stepSliderValueElement = document.getElementById("summa");
+var dataValueElement = document.getElementById("data");
+var returnValueElement = document.getElementById("return");
+
+
+var rate,valuePercent,rateValueElement = document.getElementById('percent');
+/*переменные для полученных значений ползунков*/
+var valueMoney, valueDate, valueReturn;
+
+
+
+/*полученние значений с ползунков*/
+
+function getElemantValue() {
+	valueMoney = moneySlider.noUiSlider.get();
+	valueDate = dateSlider.noUiSlider.get();
+	valueReturn = (valueMoney - (valueMoney * 90 / 100)) * valueDate;
+}
+
+
+/*вывод значений с ползунков*/
+
+dateSlider.noUiSlider.on("update", function(values, handle) {
+	getElemantValue();
+	dataValueElement.innerHTML = Math.round(valueDate);
+	returnValueElement.innerHTML = Math.round(valueReturn)+' ₽';
+});
+
+moneySlider.noUiSlider.on("update", function(values, handle) {
+	getElemantValue();
+	stepSliderValueElement.innerHTML = Math.round(valueMoney)+' ₽';
+	returnValueElement.innerHTML = Math.round(valueReturn)+' ₽';
+});
