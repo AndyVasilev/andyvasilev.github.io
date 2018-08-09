da600c94f3850590c1e5d039447dcb07642533d6
Andy Vasilev <Andy.vasilev@gmail.com>
2018-08-09 22:33:36 +0300

    karos-is

--- js/index.js
+++ js/index.js
@@ -1,136 +1,16 @@
+/* Local Storage */
+if (window.localStorage) {
+    var elements = document.querySelectorAll('[name]');
 
-var swiper = new Swiper('.section-2 .swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-          640: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-              slidesPerGroup: 1,
-          },
-          320: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-              slidesPerGroup: 1,
-          }
-      }
-    });
+    for (var i = 0, length = elements.length; i < length; i++) {
+        (function(element) {
+            var name = element.getAttribute('name');
 
+            element.value = localStorage.getItem(name) || '';
 
-    var swiper = new Swiper('.comments .slider .swiper-container', {
-        slidesPerView: 6,
-        breakpoints: {
-            414: {
-                slidesPerView: 3,
-                spaceBetween: 10,
-                slidesPerGroup: 1,
-            },
-            320: {
-                slidesPerView: 2,
-                spaceBetween: 10,
-                slidesPerGroup: 1,
-            }
-        }
-    });
-
-
-    var swiper = new Swiper('.sertificat .swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: '.sertificat .swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.sertificat .swiper-button-next',
-        prevEl: '.sertificat .swiper-button-prev',
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            slidesPerGroup: 1,
-        },
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            slidesPerGroup: 1,
-        }
+            element.onkeyup = function() {
+                localStorage.setItem(name, element.value);
+            };
+        })(elements[i]);
     }
-});
-
-
-//
-
-/*Калькулятор */
-/*Инициализация слайдеров*/
-var moneySlider = document.getElementById("slider-money");
-var dateSlider = document.getElementById("slider-date");
-
-noUiSlider.create(moneySlider, {
-	animate: true,
-	animationDuration: 300,
-	start: [0],
-	step: 300000,
-	range: {
-		min: [0],
-		max: [2000000]
-	}
-});
-noUiSlider.create(dateSlider, {
-	animate: true,
-	animationDuration: 300,
-	start: [0],
-	step: 1,
-	range: {
-		min: [0],
-		max: [10]
-	}
-});
-
-/*переменные для полей вывода чисел*/
-var stepSliderValueElement = document.getElementById("summa");
-var dataValueElement = document.getElementById("data");
-var returnValueElement = document.getElementById("return");
-
-/*переменные для полученных значений ползунков*/
-var valueMoney, valueDate, valueReturn;
-
-
-
-/*полученние значений с ползунков*/
-
-function getElemantValue() {
-	valueMoney = moneySlider.noUiSlider.get();
-	valueDate = dateSlider.noUiSlider.get();
-	valueReturn = (valueMoney - (valueMoney * 90 / 100)) * valueDate;
 }
-
-
-/*вывод значений с ползунков*/
-
-dateSlider.noUiSlider.on("update", function() {
-	getElemantValue();
-	dataValueElement.innerHTML = Math.round(valueDate).toLocaleString();
-	returnValueElement.innerHTML = Math.round(valueReturn).toLocaleString()+' ₽';
-});
-
-moneySlider.noUiSlider.on("update", function() {
-	getElemantValue();
-	stepSliderValueElement.innerHTML = Math.round(valueMoney).toLocaleString()+' ₽';
-	returnValueElement.innerHTML = Math.round(valueReturn).toLocaleString()+' ₽';
-});
